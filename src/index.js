d308a5219503cf80f4a9e26674c815e8386cf598
Simon mahns <ak.simonm@gmail.com>
2020-09-29 10:10:58 -0400

    swwutch

--- src/index.js
+++ src/index.js
@@ -6,20 +6,20 @@ import East from "./components/East";
 import Bike from "./components/Bike";
 import Lucy from "./components/Lucy";
 import SceneAlt from "./components/ThreeRenderAlt";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import Route from "react-router-dom/Route";
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <div className="app">
+        <Switch className="app">
           <Route path="/" exact strict component={SceneAlt} />
           <Route path="/east" exact strict component={East} />
           <Route path="/roadtrip" exact strict component={Lucy} />
           <Route path="/about" exact strict component={About} />
           <Route path="/bike" exact strict component={Bike} />
-        </div>
+        </Switch>
       </Router>
     );
   }
